1bf7504a332956d9372571d99c8dd5a7814dd1aa
Nandhini19126 <nandhini6002raj@gmail.com>
2026-06-29 18:44:57 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,60 @@
+from flask import Flask, render_template, request
+import tensorflow as tf
+import numpy as np
+import cv2
+import os
+
+app = Flask(__name__)
+
+# Load trained model
+model = tf.keras.models.load_model(
+    "image_classifier.h5"
+)
+
+classes = [
+    'airplane', 'automobile', 'bird', 'cat',
+    'deer', 'dog', 'frog', 'horse',
+    'ship', 'truck'
+]
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict', methods=['POST'])
+def predict():
+
+    file = request.files['image']
+
+    filepath = os.path.join(
+        "static",
+        file.filename
+    )
+
+    file.save(filepath)
+
+    img = cv2.imread(filepath)
+    img = cv2.resize(img, (32, 32))
+    img = img / 255.0
+
+    img = np.expand_dims(img, axis=0)
+
+    prediction = model.predict(img)
+
+    class_index = np.argmax(prediction)
+    result = classes[class_index]
+
+    confidence = round(
+        np.max(prediction) * 100,
+        2
+    )
+
+    return render_template(
+        'index.html',
+        prediction=result,
+        confidence=confidence,
+        image_path=filepath
+    )
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ train_model.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+
+# Load CIFAR-10 Dataset
+(x_train, y_train), (x_test, y_test) = datasets.cifar10.load_data()
+
+# Normalize Images
+x_train = x_train / 255.0
+x_test = x_test / 255.0
+
+# CNN Model
+model = models.Sequential([
+    layers.Conv2D(32, (3,3), activation='relu',
+                  input_shape=(32,32,3)),
+    layers.MaxPooling2D((2,2)),
+
+    layers.Conv2D(64, (3,3), activation='relu'),
+    layers.MaxPooling2D((2,2)),
+
+    layers.Conv2D(64, (3,3), activation='relu'),
+
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile Model
+model.compile(
+    optimizer='adam',
+    loss='sparse_categorical_crossentropy',
+    metrics=['accuracy']
+)
+
+# Train Model
+model.fit(x_train, y_train, epochs=10,
+          validation_data=(x_test, y_test))
+
+# Save Model
+model.save("image_classifier.h5")
+
+print("Model Saved Successfully!")
